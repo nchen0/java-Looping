629b6d9b2730305ccdddf64f2db384717cbd20fd
nchen0 <nicky.chen0@gmail.com>
2019-02-06 21:34:33 -0500

    add loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,5 +18,68 @@ public class Main {
             System.out.println("Random number " + rand.nextInt());
             System.out.println("Random number " + rand.nextInt(5));
         }
+
+        System.out.println("\nRandom Doubles");
+        // While Loop
+        int i = 0;
+        while (i < 7) {
+            System.out.println("Random number " + rand.nextDouble());
+            i++;
+        }
+
+        // do While Loop
+        System.out.println(("\nRandom Integers until 0"));
+        int myTest; // block scope. Do while loop always executes AT LEAST once, unlike whiles.
+        do {
+            myTest = rand.nextInt(7);
+            System.out.println("Number " + myTest);
+        } while (myTest != 0);
+
+        // Conditions
+        System.out.println("\nRandom Even Numbers");
+        for (int j = 0; j < 10; j++) {
+            int myTester = rand.nextInt(10);
+            System.out.println("Testing " + myTester);
+            if ((myTester % 2) == 0) {
+                break;
+            }
+            System.out.println(myTester);
+        }
+
+        System.out.println("\nPrint Days");
+        int randDay = rand.nextInt(7);
+        if ((randDay == 0) || (randDay == 6)) {
+            System.out.println("Weekend");
+        } else {
+            System.out.println("Weekday");
+        }
+
+        System.out.println("\nPrint Days");
+        randDay = rand.nextInt(3);
+        if (randDay == 0) {
+            System.out.println("Sunday");
+        } else if (randDay == 1) {
+            System.out.println("Monday");
+        } else if (randDay == 2) {
+            System.out.println("Tuesday");
+        } else {
+            System.out.println("Error");
+        }
+
+        System.out.println("\nSwitch Days");
+        switch(randDay) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Error");
+        }
+
     }
 }
